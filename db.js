fad6738b61b9366ed80102e8161ee23b940ebddf
Hizkia Haezer <145329254+HizkiaHaezer@users.noreply.github.com>
2024-03-27 21:50:15 +0800

    ex8

---
+++ db.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'postgres',
+  password: '050304',
+  host: 'localhost',
+  port: 5432, 
+  database: 'bedatabase'
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params)
+};
---
+++ users.js
@@ -0,0 +1,16 @@
+users = [
+    {
+        id: 1,
+        name: "Andre"
+    },
+    {
+        id: 2,
+        name: "SUle"
+    },
+    {
+        id: 3,
+        name: "Surya"
+    }
+]
+
+module.exports = users
